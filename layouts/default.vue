a5736946c39c950844b38c0300c337a6768fb510
Kenneth Illman <kenneth.illman@gmail.com>
2020-06-12 10:00:23 +0530

    Seo desc + key

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,6 +15,37 @@ export default {
     SiteHeader,
     SiteFooter
   },
+ data() {
+    return {
+      seo_desc_en: this.$store.state.footer_en.seo_description[0].text,
+      seo_keys_en: this.$store.state.footer_en.seo_keywords[0].text,
+      seo_desc_sv: this.$store.state.footer_sv.seo_description[0].text,
+      seo_keys_sv: this.$store.state.footer_sv.seo_keywords[0].text,
+      seo_lang: this.$i18n.locale,
+    };
+  },
+  head () {
+
+    let meta = [] 
+
+    if (this.seo_lang === 'en') {
+      meta = [
+        { name: 'description', content: this.seo_desc_en },
+        { name: 'keywords', content: this.seo_keys_en },
+      ];
+    }
+
+    if (this.seo_lang === 'sv') {
+      meta = [
+        { name: 'description', content: this.seo_desc_sv },
+        { name: 'keywords', content: this.seo_keys_sv },
+      ];
+    }
+
+    return {
+      meta: meta,
+    }
+  },
   // Called before rendering the layout (even for error page)
   async middleware({ store, $prismic, app }) {
     // console.log('middleware-> ' , app)
